41a05ceaa2e8288f4f0e84f6b7d55e755f276ecb
Ryan Blahnik <ryanblahnik@gmail.com>
2019-02-16 00:13:14 -0600

    Promisify

--- async.js
+++ async.js
@@ -1,3 +1,5 @@
+console.log('xx: ', Date.now());
+
 const delayed = () => {
   return setTimeout(function() {
     console.log(`3: 'no'`);
@@ -9,10 +11,10 @@ const weird = () => {
   return new Promise((resolve, reject) => {
     return setTimeout(function() {
       console.log(`11: 'no'`);
-      // console.log("line 34 would never complete\n" +
+      // console.log("line 51 would never complete\n" +
       //   "if we don't resolve or reject");
-      reject('14: resolved');
-      resolve('15: rejected');
+      resolve('14: rejected');
+      reject('15: resolved');
     }, 1000);
   });
 };
@@ -26,28 +28,77 @@ const promised = () => {
   });
 };
 
-weird()
-  .then((data) => console.log('30: ', data))
-  .catch((err) => console.log('31: ', err));
+// weird()
+//   .then((data) => console.log('30: ', data))
+//   .catch((err) => console.log('31: ', err));
 
 // const array = [weird(), promised()];
 // console.log(array);
 
-Promise.all([weird(), promised()])
-  .then(console.log)
-  .catch(console.log);
+// Promise.all(array)
+//   .then((results) => {
+//     console.log('results received');
+//     results.forEach((result) => console.log(result))
+//     console.log('results logged');
+//     console.log(array);
+//     console.log('array logged');
+//   })
+//   .catch(console.log);
 
 const example = async () => {
-  console.log('28: ', Date.now());
+  console.log('47: ', Date.now());
   try {
     const value0 = await weird();
-    console.log('value0 (35): ', value0);
+    console.log('value0 (50): ', value0);
   } catch(err) {
-    console.log('err (37): ', err);
+    console.log('err (52): ', err);
   }
   const value = await promised();
   console.log(value);
-  console.log('41: ', Date.now());
+  console.log('56: ', Date.now());
 };
 
 // example();
+
+const oldStyle = (callback) => {
+  console.log('64: welcome back');
+  let err = null;
+  let data = null;
+  if (Math.random() < 0.5) {
+    err = 'noo';
+  } else {
+    data = 'winner';
+  }
+  if (err) return callback(err, null);
+  return callback(null, data);
+};
+
+oldStyle((err, data) => {
+  if (err) return console.error('77:', err);
+  return console.log('78: Yes!! ', data);
+});
+
+// console.log('81: ', Date.now());
+
+const makeNewStyle = (fn) => {
+  return function() {
+    return new Promise((resolve, reject) => {
+      let args = Array.from(arguments);
+      let cbHandling = (err, data) => {
+        if (err) return reject('88: ' + err);
+        return resolve('89: ' + data);
+      };
+      args.push(cbHandling);
+      return fn(...args);
+    });
+  };
+};
+
+console.log('97: ', Date.now());
+const newStyle = makeNewStyle(oldStyle);
+
+newStyle()
+  .then((data) => console.log('01: ', data, '\n^^: ', Date.now()))
+  .catch((err) => console.error('02: ', err, '\n^^: ', Date.now()));
+
+console.log('04: ', Date.now());
