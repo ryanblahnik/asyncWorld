907768a8e88a8567b7d3930a1d8221458fbdeb5c
Ryan Blahnik <ryanblahnik@gmail.com>
2019-02-16 00:28:18 -0600

    Add another async wrapper

--- async.js
+++ async.js
@@ -2,7 +2,7 @@ console.log('xx: ', Date.now());
 
 const delayed = () => {
   return setTimeout(function() {
-    console.log(`3: 'no'`);
+    console.log(`5: 'no'`);
     // console.log(Date.now());
   }, 1000)
 }
@@ -10,11 +10,11 @@ const delayed = () => {
 const weird = () => {
   return new Promise((resolve, reject) => {
     return setTimeout(function() {
-      console.log(`11: 'no'`);
+      // console.log(`13: 'no'`);
       // console.log("line 51 would never complete\n" +
       //   "if we don't resolve or reject");
-      resolve('14: rejected');
-      reject('15: resolved');
+      resolve(`16:  'rejected'`);
+      reject(`17:  'resolved'`);
     }, 1000);
   });
 };
@@ -22,7 +22,7 @@ const weird = () => {
 const promised = () => {
   return new Promise((resolve, reject) => {
     return setTimeout(function() {
-      resolve(`23: 'no'`);
+      resolve(`25:  'no'`);
       reject('nono');
     }, 1000);
   });
@@ -33,35 +33,39 @@ const promised = () => {
 //   .catch((err) => console.log('31: ', err));
 
 // const array = [weird(), promised()];
-// console.log(array);
+const array02 = [];
+// console.log(array); // Promises <pending>
 
 // Promise.all(array)
 //   .then((results) => {
-//     console.log('results received');
-//     results.forEach((result) => console.log(result))
-//     console.log('results logged');
-//     console.log(array);
-//     console.log('array logged');
+//     // console.log('results received');
+//     results.forEach((result) => {
+//       console.log(result)
+//       array02.push(result);
+//     });
+//     // console.log('results logged');
+//     // console.log(array);
+//     // console.log('array logged');
 //   })
-//   .catch(console.log);
+//   .catch(console.error);
 
 const example = async () => {
-  console.log('47: ', Date.now());
+  console.log('53: ', Date.now());
   try {
     const value0 = await weird();
-    console.log('value0 (50): ', value0);
+    console.log('value0 (56): ', value0);
   } catch(err) {
-    console.log('err (52): ', err);
+    console.log('err (57): ', err);
   }
   const value = await promised();
   console.log(value);
-  console.log('56: ', Date.now());
+  console.log('62: ', Date.now());
 };
 
 // example();
 
 const oldStyle = (callback) => {
-  console.log('64: welcome back');
+  // console.log('68:  welcome back');
   let err = null;
   let data = null;
   if (Math.random() < 0.5) {
@@ -73,10 +77,10 @@ const oldStyle = (callback) => {
   return callback(null, data);
 };
 
-oldStyle((err, data) => {
-  if (err) return console.error('77:', err);
-  return console.log('78: Yes!! ', data);
-});
+// oldStyle((err, data) => {
+//   if (err) return console.error('77:', err);
+//   return console.log('78: Yes!! ', data);
+// });
 
 // console.log('81: ', Date.now());
 
@@ -85,8 +89,8 @@ const makeNewStyle = (fn) => {
     return new Promise((resolve, reject) => {
       let args = Array.from(arguments);
       let cbHandling = (err, data) => {
-        if (err) return reject('88: ' + err);
-        return resolve('89: ' + data);
+        if (err) return reject('92:  ' + err);
+        return resolve('93:  ' + data);
       };
       args.push(cbHandling);
       return fn(...args);
@@ -94,11 +98,29 @@ const makeNewStyle = (fn) => {
   };
 };
 
-console.log('97: ', Date.now());
+// console.log('01: ', Date.now());
 const newStyle = makeNewStyle(oldStyle);
 
-newStyle()
-  .then((data) => console.log('01: ', data, '\n^^: ', Date.now()))
-  .catch((err) => console.error('02: ', err, '\n^^: ', Date.now()));
+// newStyle()
+//   .then((data) => console.log('05: ', data, '\n^^: ', Date.now()))
+//   .catch((err) => console.error('06: ', err, '\n^^: ', Date.now()));
 
-console.log('04: ', Date.now());
+// console.log('08: ', Date.now());
+
+// setTimeout(() => {
+//   console.log(array);
+//   console.log(array02);
+// }, 3000);
+
+const wrapper = async () => {
+  try {
+    const output = await newStyle();
+    console.log(output);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+wrapper();
+
+console.log('26: ', Date.now());
